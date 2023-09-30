25c85c82f202f1a97db09c702c0be6d8f556093f
Muhammad Qadri <mqad21@gmail.com>
2023-09-30 14:05:57 +0700

    fix: no primary key on models

--- src/Database/Models/Menu.ts
+++ src/Database/Models/Menu.ts
@@ -2,12 +2,18 @@ import { DataTypes, ModelAttributes } from "sequelize"
 import { Model } from "../../Structures"
 
 export class Menu extends Model {
-
+    
+    declare id: number
     declare clientId: string
     declare jid: string
     declare menu?: string | null
 
     static attributes: ModelAttributes = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
         clientId: {
             type: DataTypes.STRING,
             allowNull: false
--- src/Database/Models/State.ts
+++ src/Database/Models/State.ts
@@ -3,11 +3,17 @@ import { Model } from "../../Structures"
 
 export class State extends Model {
 
+    declare id: number
     declare clientId: string
     declare jid: string
     declare state?: string | null
 
     static attributes: ModelAttributes = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
         clientId: {
             type: DataTypes.STRING,
             allowNull: false
